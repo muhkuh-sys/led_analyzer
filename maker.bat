1f7214357d7445aa38347349e4f5e3d03fe86140
Subhan <swaizi@hilscher.com>
2015-03-06 11:14:41 +0100

    committing all

---
+++ maker.bat
@@ -0,0 +1,10 @@
+
+mkdir testfolder
+cd testfolder
+cmake -G "MinGW Makefiles" -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_C_COMPILER=C:/TDM-GCC-64/bin/x86_64-w64-mingw32-gcc.exe -DCMAKE_CXX_COMPILER=C:/TDM-GCC-64/bin/x86_64-w64-mingw32-g++.exe ..
+
+cd..
+c:\TDM-GCC-64\bin\mingw32-make.exe 
+
+PAUSE
+
