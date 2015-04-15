9ff24b93b43d5a88b20394d10fb5ce3504b23290
Subhan <subhan@SubhanVM.(none)>
2015-04-15 15:45:13 +0200

    Changed folder names

--- .build.sh
+++ .build.sh
@@ -2,9 +2,9 @@
 
 
 ## Build the 32 Bit Version
-rm -rf build_win32
-mkdir build_win32
-cd build_win32
+rm -rf build_windows32
+mkdir build_windows32
+cd build_windows32
 
 cmake -DUSE_SYSTEM_LUA=FALSE -DCMAKE_C_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 -DCMAKE_EXE_LINKER_FLAGS=-m32 -DCMAKE_MODULE_LINKER_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=/usr/bin/i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/i686-w64-mingw32-g++ ..
 
@@ -14,9 +14,9 @@ make
 cd ..
 
 ## Build the 64 Bit Version
-rm -rf build_win64
-mkdir build_win64
-cd build_win64
+rm -rf build_windows64
+mkdir build_windows64
+cd build_windows64
 
 cmake -DUSE_SYSTEM_LUA=FALSE -DCMAKE_C_FLAGS=-m64 -DCMAKE_SHARED_LINKER_FLAGS=-m64 -DCMAKE_EXE_LINKER_FLAGS=-m64 -DCMAKE_MODULE_LINKER_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ ..
 
