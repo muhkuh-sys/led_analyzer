b4bd7ee9eb9bbc7056e9e5e8814ff96b7db1d1b2
Subhan <swaizi@hilscher.com>
2015-04-22 15:45:07 +0200

    testing main function

--- modules/led_analyzer/runme.lua
+++ modules/led_analyzer/runme.lua
@@ -146,18 +146,14 @@ else
 			error_counter = 0 
 		end 
 		
+	
+	led_analyzer.set_gain(apHandles, devIndex, TCS3471_GAIN_4X)
+	led_analyzer.set_intTime(apHandles, devIndex, TCS3471_INTEGRATION_200ms)
+	
 		
+	led_analyzer.get_gain(apHandles, devIndex, aucGains)
+	led_analyzer.get_intTime(apHandles, devIndex, aucIntTimes)
 		
-		--led_analyzer.set_gain_x(apHandles, devIndex, TCS3471_GAIN_4X, 0);
-		--led_analyzer.set_intTime_x(apHandles, devIndex, TCS3471_INTEGRATION_154ms, 0);
-		
-
-			led_analyzer.set_gain(apHandles, devIndex, TCS3471_GAIN_4X)
-			led_analyzer.set_intTime(apHandles, devIndex, TCS3471_INTEGRATION_200ms)
-
-			led_analyzer.get_gain(apHandles, devIndex, aucGains)
-			led_analyzer.get_intTime(apHandles, devIndex, aucIntTimes)
-			
 --			for i = 0, 15 do
 --				print(led_analyzer.puchar_getitem(aucGains, i))
 --				print(led_analyzer.puchar_getitem(aucIntTimes, i))
