0c0252f4c289fb6040a2ae2f53ef81078cd97da5
Subhan <swaizi@hilscher.com>
2015-04-28 15:43:19 +0200

    Color validation now distinguishes between different errors in LED - recognizing

--- modules/led_analyzer/color_validation.lua
+++ modules/led_analyzer/color_validation.lua
@@ -1,14 +1,15 @@
 
 
--- calculate the distance between 2 points v1 and v2  
-function get_distance(v1, v2)
-	return math.sqrt(math.pow((v1.x - v2.x),2) + math.pow((v1.y - v2.y),2))
-end 
 
 
 -- tTest = a row in the tTesttable containing name, dWavelength, tol, led_state ...
 -- tnm is a row with a .nm value 
-function inToleranceRange(tTest, tnm, tYxy) 
+
+local tTestSummary = {}
+
+function inToleranceRange(devIndex, tTest, tWavelength, brightness_enable) 
+	
+	
 	
 	
 	
@@ -26,118 +27,126 @@ function inToleranceRange(tTest, tnm, tYxy)
 				else 
 					return 2
 				end 
-			end
-			
-			
-		
-	else
-		if tTest.x ~= nil and tTest.y ~= nil then
-			
-			distance = get_distance(tTest, tYxy)
-			if distance <= tTest.tol_xy then
-				return 0 
-			else 
-				return 1
 			end 
-							
+	end
+			
+			
+end 
+
+function compare(row_testboard, row_curset, brightness_check_enable)
+	
+	-- The row in testboard exists, thus a test for this LED is desired 
+	if row_testboard.nm ~= nil then 
+		local dnm = row_curset.nm - row_testboard.nm 
+		local dsat = row_curset.sat - row_testboard.sat
+		local dbrightness 
+		
+		if brightness_check_enable >= 1 then 
+			dbrightness = row_curset.brightness - row_testboard.brightness 
 		else 
-			print("Table needs input values! Either dom. wavelength or x/y points")
+			dbrightness = 0 
 		end 
+		
+		-- Saturation and wavelength fit 
+		if  row_curset.nm  		  > (row_testboard.nm  - row_testboard.tol_nm)
+		and row_curset.nm  		  < (row_testboard.nm  + row_testboard.tol_nm)
+		and row_curset.sat 		  > (row_testboard.sat - row_testboard.tol_sat)
+		and row_curset.sat		  < (row_testboard.sat + row_testboard.tol_sat) then 
+		
+			-- Brightness check is enabled 
+			if brightness_check_enable >= 1 then 
+				-- Brightness is OK --
+				if row_curset.brightness > (row_testboard.brightness - row_testboard.tol_brightness)
+				and row_curset.brightness < (row_testboard.brightness + row_testboard.tol_brightness) then 
+					print("hello")
+					return 0, "OK", dnm, dsat, dbrightness
+				 
+				-- Brightness falls below min_brightness
+				elseif row_curset.brightness < (row_testboard.brightness - row_testboard.tol_brightness) then
+					return 1, "LED too tark -- Check if right series resisor is used", dnm, dsat, dbrightness
+				 
+				-- Brightness exceeds max_brightness
+				elseif row_curset.brightness > (row_testboard.brightness + row_testboard.tol_brightness) then  
+					return 2, "LED too bright -- Check if right series resistor is used or shortcuts exists on the board", dnm, dsat, dbrightness
+				end   
+			
+			--Brightness check is disabled 
+			else 
+				-- As wavelength and saturations are OK and there's no need for a brightness check we can return OK here
+				return 0, "OK", 0, 0, 0
+			end 
+		
+		-- Saturation fits but wavelength doesn't 
+		elseif (row_curset.nm  < (row_testboard.nm  - row_testboard.tol_nm) 
+		or 	   row_curset.nm  > (row_testboard.nm  + row_testboard.tol_nm))
+		and    row_curset.sat > (row_testboard.sat - row_testboard.tol_sat)
+		and    row_curset.sat < (row_testboard.sat + row_testboard.tol_sat) then 
+			-- We probly have a wrong LED in place 
+			return 3, "A LED with wrong color is detected - check !", dnm, dsat, dbrightness
+			
+		-- Neither saturation nor wavelength fit -- NO LED 
+		else 
+			return 4, "NO LED detected!", dnm, dsat, dbrightness
+		end 
+	
+	-- the wavelength field in the row for testboard does not exist, thus we do not need to test this LED "
+	else 
+		return 0, "NO TEST", 0, 0, 0
 	end 
 	
 end 
-
-
-function validate_color(tTest, tnm, tYxy)
 	
-	
-	for k,v in ipairs(tTest) do
-			if inToleranceRange(tTest[k], tnm[k], tYxy[k]) == 0 then 
-				tTest[k].LED_State = 1
-			elseif inToleranceRange(tTest[k], tnm[k], tYxy[k]) == 1 then
-			    tTest[k].LED_State = 2 			 
-		    end 
-	end 	
 	 
+-- Checks if the LEDs measured by a device are correct 
+function inRange(tTestDevice, tWavelengthDevice, brightness_check_enable)
+	local tTestSummary_device = {}
+	
+	local status, infotext, dnm, dsat, dbrightness
+	for sensor = 1, 16 do 
+		tTestSummary_device[sensor] = {}
+		
+		status, infotext, dnm, dsat, dbrightness = compare(tTestDevice[sensor], tWavelengthDevice[sensor], brightness_check_enable)
+		
+		tTestSummary_device[sensor].status = status
+		tTestSummary_device[sensor].infotext = infotext
+		-- measurement error = x_measured - x_correct
+		tTestSummary_device[sensor].dnm  = dnm 
+		tTestSummary_device[sensor].dsat = dsat  
+		tTestSummary_device[sensor].dbrightness = dbrightness  
+	end 
+	
+	return tTestSummary_device 
+	
 end 
-	-- k = 1 
-			-- if inToleranceRange(tTest[k], tnm[k], tYxy[k]) == 0 then 
-				-- tTest[k].LED_State = 1
-			
-			-- elseif inToleranceRange(tTest[k], tnm[k], tYxy[k]) == 1 then
-				-- tTest[k].LED_State = 2 		
-			-- end 
 
-	-- k = 8
-			-- if inToleranceRange(tTest[k], tnm[k], tYxy[k]) == 0 then 
-				-- tTest[k].LED_State = 1
-			
-			-- elseif inToleranceRange(tTest[k], tnm[k], tYxy[k]) == 1 then
-				-- tTest[k].LED_State = 2 
-			-- end 
-	
-	-- k  = 13 
-	
-			-- if inToleranceRange(tTest[k], tnm[k], tYxy[k]) == 0 then 
-				-- tTest[k].LED_State = 1
-			
-			-- elseif inToleranceRange(tTest[k], tnm[k], tYxy[k]) == 1 then
-				-- tTest[k].LED_State = 2 
-			-- end 
-	
---end 
 
-function print_LEDs(tTest)
-	local flag = 0
-	for k,v in ipairs(tTest) do
-		if tTest[k].LED_State ~=nil then
-			if tTest[k].LED_State == 1 then
-			 print(string.format("%i - %s found.", k, tTest[k].name))			
-			
-			
-			 elseif tTest[k].LED_State == 2 then
-		     print(string.format("%i, %s color recognized, but purity out of tolerance range.", k, tTest[k].name))
-				
-			else 
-			 print(string.format("%i, %s NOT FOUND.", k, tTest[k].name))
-				 flag = 0
-			 end 
-		 end
-	 end
- end 
+function validate_device_colors(tTestDevice, tWavelengthDevice, brightness_check_enable)
 
--- function print_LEDs(tTest)
--- k = 1
-			-- if tTest[k].LED_State == 1 then
-				-- print(string.format("%s found.", tTest[k].name))	
-			
-			-- elseif tTest[k].LED_State == 2 then
-				-- print(string.format("%s color recognized, but purity out of tolerance range.", tTest[k].name))
-			
-			-- else 
-				-- print(string.format("%s NOT FOUND.", tTest[k].name))
-			-- end 
-			
--- k = 8
-			-- if tTest[k].LED_State == 1 then
-				-- print(string.format("%s found.", tTest[k].name))	
-			
-			-- elseif tTest[k].LED_State == 2 then
-				-- print(string.format("%s color recognized, but purity out of tolerance range.", tTest[k].name))
-			
-			-- else 
-				-- print(string.format("%s NOT FOUND.", tTest[k].name))
-			-- end 
-			
--- k = 13 
-			-- if tTest[k].LED_State == 1 then
-				-- print(string.format("%s found.", tTest[k].name))	
-			
-			-- elseif tTest[k].LED_State == 2 then
-				-- print(string.format("%s color recognized, but purity out of tolerance range.", tTest[k].name))
-			
-			-- else 
-				-- print(string.format("%s NOT FOUND.", tTest[k].name))
-			-- end 
--- end 
+		local tTestSummary_device = {} 
+		tTestSummary_device = inRange(tTestDevice, tWavelengthDevice, brightness_check_enable)
+	 
+	 
+		return tTestSummary_device 
+end 
+
+
+-- prints a test summary for a device 
+function print_deviceSummary(tTestSummary_device, info_enable)
+
+	if info_enable >= 0 then 
+		print("Status --------- dnm ----------- dsat -------- dbrightness ------ infotext -----")
+	else 
+		print("Status --------- dnm ----------- dsat -------- dbrightness -----------------")
+	end 
 	
+	for i = 1, 16 do 
+		if info_enable >= 1 then 
+			print(string.format("%2d		%3d		%2.2f		%1.5f		%s", tTestSummary_device[i].status, tTestSummary_device[i].dnm, 
+			tTestSummary_device[i].dsat, tTestSummary_device[i].dbrightness, tTestSummary_device[i].infotext))
+		else 
+			print(string.format("%2d		%3d		%2.2f		%1.5f", tTestSummary_device[i].status, tTestSummary_device[i].dnm, 
+			tTestSummary_device[i].dsat, tTestSummary_device[i].dbrightness))
+		end 
+	end 
+
+end 
