1a7823024b303e36905dde9fd3ffeeffb37c7040
Subhan <swaizi@hilscher.com>
2015-05-05 16:47:10 +0200

    Play with testboards

--- modules/led_analyzer/testboard.lua
+++ modules/led_analyzer/testboard.lua
@@ -6,75 +6,53 @@ tTest =
 {
 -- maximum of 16 entries per device 
 	[0] = { 
+			[1]  =  {name = "", nm = 500, tol_nm = 10, sat = 100, tol_sat = 10, lux = 1200, tol_lux = 250},
+							   		
+			[2]  =  {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},			
+							   		                               
+			[3]  =  {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                               
+			[4]  =  {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+                                                                            
+			[5]  =  {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                               
+			[6]  =  {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                               
+			[7]  =  {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                               
+			[8]  =  {name = "", nm = 550, tol_nm = 9, sat = 95,   tol_sat = 10, lux = 0.0005, tol_lux = 20},
+			                                                                
+			[9]  =  {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                         
+			[10] = {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                         
+			[11] = {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		      							   		                         
+			[12] = {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                         
+			[13] = {name = "", nm = 550, tol_nm = 5, sat = 70,   tol_sat = 5,  lux = 0.0005, tol_lux = 20},
+							   		                         
+			[14] = {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                         
+			[15] = {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},										   
+			                                                                
+			[16] = {name = "", nm = 550, tol_nm = 5, sat = 70,   tol_sat = 5,  lux = 0.0005, tol_lux = 20}										  
+		 },
+		 
 
-			[1] =  {name = "", nm = 459 , tol_nm = 10, sat = 100,  tol_sat = 10, lux = 0.02, tol_lux = 0.01},
-											
-			[2] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},			
-											
-			[3] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											
-			[4] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-
-			[5] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											
-			[6] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											
-			[7] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											
-			[8] =  {name = "", nm = 571.0, tol_nm = 9, sat = 95,  tol_sat = 10, lux = 0.0005, tol_lux = 20},
-			
-			[9] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													
-			[10] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													
-			[11] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													
-			[12] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													
-			[13] = {name = "", nm = 596, tol_nm = 5,sat = 70,  tol_sat = 5, lux = 0.0005, tol_lux = 20},
-													
-			[14] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													
-			[15] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},										   
-			
-			[16] = {name = "", nm = 596, tol_nm = 5,sat = 70,  tol_sat = 5, lux = 0.0005, tol_lux = 20}
-		},
-			
-
-	  -- table for second device and so on ...., max 16 entries per device index
-	[1] = {
-			[1] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											   
-			[2] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},			
-											   
-			[3] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											   
-			[4] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											   
-			[5] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											   
-			[6] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											   
-			[7] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-											   
-			[8] =  {name = "", nm = 571, tol_nm = 9, sat = 95,  tol_sat = 10, lux = 0.0005, tol_lux = 20},
-			
-			[9] =  {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													   
-			[10] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													   
-			[11] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													   
-			[12] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													   
-			[13] = {name = "", nm = 596, tol_nm = 5,sat = 70,  tol_sat = 5, lux = 0.0005, tol_lux = 20},
-													  
-			[14] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},
-													   
-			[15] = {name = "", nm = 615 , tol_nm = 8, sat = 60,  tol_sat = 2, lux = 0.0005, tol_lux = 20},										   
-			
-			[16] = {name = "", nm = 596, tol_nm = 5,sat = 70,  tol_sat = 5, lux = 0.0005, tol_lux = 20}
-		  }
-				
+	[2] = { 
+							   		                               
+			[8] =  {name = "", nm = 550, tol_nm = 9, sat = 95,   tol_sat = 10, lux = 0.0005, tol_lux = 20},
+							   		                         
+			[12] = {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                         
+			[13] = {name = "", nm = 550, tol_nm = 5, sat = 70,   tol_sat = 5,  lux = 0.0005, tol_lux = 20},
+							   		                         
+			[14] = {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},
+							   		                         
+			[15] = {name = "", nm = 550, tol_nm = 8, sat = 60,   tol_sat = 2,  lux = 0.0005, tol_lux = 20},										   
+			                                                                
+			[16] = {name = "", nm = 550, tol_nm = 5, sat = 70,   tol_sat = 5,  lux = 0.0005, tol_lux = 20}
+		 }
 }
 
