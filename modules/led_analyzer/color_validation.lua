b0d77c3613839ecb7bd4114be9f95f53d76a09e2
Subhan <swaizi@hilscher.com>
2015-05-06 17:24:27 +0200

    different warning levels on measurements

--- modules/led_analyzer/color_validation.lua
+++ modules/led_analyzer/color_validation.lua
@@ -1,58 +1,64 @@
+-- WARNING LEVELS
+-- 0 : no warnings if test entries are missing for a device or test entries are missing for sensors
+-- 1 : prints out warnings if test entries are missing for devices or sensors 
+-- 2 : if test entries for devices miss we quit
+-- 3 : if test entries for devices are missing we cry ... alot ... very loud
 
+WARNING_LEVEL = 1
 
--- tReferenceColorSensor is a row in testboard.lua which contains LED, sat, lux and nm values and tolerances
+-- tDUTrow is a row in testboard.lua which contains LED, sat, lux and nm values and tolerances
 -- tnm is a row with a .nm value 
 
-function compareRows(tReferenceColorSensor, tCurColorSensor, lux_check_enable)
+function compareRows(tDUTrow, tCurColorSensor, lux_check_enable)
 	
 	-- The row in testboard exists, thus a test for this LED is desired 
-	if tReferenceColorSensor ~= nil then 
+	if tDUTrow ~= nil then 
 		-- d prefix describes a delta value 
-		local dnm = tCurColorSensor.nm - tReferenceColorSensor.nm 
-		local dsat = tCurColorSensor.sat - tReferenceColorSensor.sat
+		local dnm = tCurColorSensor.nm - tDUTrow.nm 
+		local dsat = tCurColorSensor.sat - tDUTrow.sat
 		local dlux 
 		
 		if lux_check_enable >= 1 then 
-			dlux = tCurColorSensor.lux - tReferenceColorSensor.lux 
+			dlux = tCurColorSensor.lux - tDUTrow.lux 
 		else 
 			dlux = 0 
 		end 
 		
 		-- Saturation and wavelength fit 
-		if  (tCurColorSensor.nm  		  >= (tReferenceColorSensor.nm  - tReferenceColorSensor.tol_nm)
-		and  tCurColorSensor.nm  		  <= (tReferenceColorSensor.nm  + tReferenceColorSensor.tol_nm)
-		and  tCurColorSensor.sat 		  >= (tReferenceColorSensor.sat - tReferenceColorSensor.tol_sat)
-		and  tCurColorSensor.sat		  <= (tReferenceColorSensor.sat + tReferenceColorSensor.tol_sat)) then 
+		if  (tCurColorSensor.nm  		  >= (tDUTrow.nm  - tDUTrow.tol_nm)
+		and  tCurColorSensor.nm  		  <= (tDUTrow.nm  + tDUTrow.tol_nm)
+		and  tCurColorSensor.sat 		  >= (tDUTrow.sat - tDUTrow.tol_sat)
+		and  tCurColorSensor.sat		  <= (tDUTrow.sat + tDUTrow.tol_sat)) then 
 		
 			-- Brightness check is enabled 
 			if lux_check_enable >= 1 then 
 				-- Brightness is OK --
-				if tCurColorSensor.lux > (tReferenceColorSensor.lux - tReferenceColorSensor.tol_lux)
-				and tCurColorSensor.lux < (tReferenceColorSensor.lux + tReferenceColorSensor.tol_lux) then 
-					return 0, string.format("%s LED with %d nm OK - PASS!\n", tReferenceLEDcolors[tReferenceColorSensor.nm].colorname, tCurColorSensor.nm), dnm, dsat, dlux
+				if tCurColorSensor.lux > (tDUTrow.lux - tDUTrow.tol_lux)
+				and tCurColorSensor.lux < (tDUTrow.lux + tDUTrow.tol_lux) then 
+					return 0, string.format("%s LED with %3d nm OK - PASS!\n", tReferenceLEDcolors[tDUTrow.nm].colorname, tCurColorSensor.nm), dnm, dsat, dlux
 				 
 				-- Brightness falls below min_brightness
-				elseif tCurColorSensor.lux < (tReferenceColorSensor.lux - tReferenceColorSensor.tol_lux) then
+				elseif tCurColorSensor.lux < (tDUTrow.lux - tDUTrow.tol_lux) then
 					return 1, "LED too dark -- Check if right series resisor is used\n", dnm, dsat, dlux
 				 
 				-- Brightness exceeds max_brightness
-				elseif tCurColorSensor.lux > (tReferenceColorSensor.lux + tReferenceColorSensor.tol_lux) then  
+				elseif tCurColorSensor.lux > (tDUTrow.lux + tDUTrow.tol_lux) then  
 					return 2, "LED too bright -- Check if right series resistor is used or shortcuts exists on the board\n", dnm, dsat, dlux
 				end   
 			
 			--Brightness check is disabled 
 			else 
 				-- As wavelength and saturations are OK and there's no need for a lux check we can return OK here
-				return 0, string.format("%s LED with %d nm OK - PASS!", tReferenceLEDcolors[tReferenceColorSensor.nm].colorname, tCurColorSensor.nm), dnm, dsat, dlux
+				return 0, string.format("%s LED with %3d nm OK - PASS!", tReferenceLEDcolors[tDUTrow.nm].colorname, tCurColorSensor.nm), dnm, dsat, dlux
 			end 
 		
 		-- Saturation fits but wavelength doesn't 
-		elseif ((tCurColorSensor.nm  <= (tReferenceColorSensor.nm  - tReferenceColorSensor.tol_nm) 
-		or 	     tCurColorSensor.nm  >= (tReferenceColorSensor.nm  + tReferenceColorSensor.tol_nm))
-		and      tCurColorSensor.sat >= (tReferenceColorSensor.sat - tReferenceColorSensor.tol_sat)
-		and      tCurColorSensor.sat <= (tReferenceColorSensor.sat + tReferenceColorSensor.tol_sat)) then 
-			return 3, string.format("Wrong LED Color! Want: %s with %d nm -- Detected: %s with %d nm\n", tReferenceLEDcolors[tReferenceColorSensor.nm].colorname,
-					tReferenceLEDcolors[tReferenceColorSensor.nm].nm, tReferenceLEDcolors[tCurColorSensor.nm].colorname, tReferenceLEDcolors[tCurColorSensor.nm].nm), dnm, dsat, dlux
+		elseif ((tCurColorSensor.nm  <= (tDUTrow.nm  - tDUTrow.tol_nm) 
+		or 	     tCurColorSensor.nm  >= (tDUTrow.nm  + tDUTrow.tol_nm))
+		and      tCurColorSensor.sat >= (tDUTrow.sat - tDUTrow.tol_sat)
+		and      tCurColorSensor.sat <= (tDUTrow.sat + tDUTrow.tol_sat)) then 
+			return 3, string.format("Wrong LED Color! Want: %s with %3d nm -- Detected: %s with %3d nm\n", tReferenceLEDcolors[tDUTrow.nm].colorname,
+					tReferenceLEDcolors[tDUTrow.nm].nm, tReferenceLEDcolors[tCurColorSensor.nm].colorname, tReferenceLEDcolors[tCurColorSensor.nm].nm), dnm, dsat, dlux
 			
 		-- Neither saturation nor wavelength fit -- NO LED 
 		else 
@@ -68,14 +74,14 @@ end
 	
 	 
 -- Checks if the LEDs measured by a device are correct 
-function inRange(tReferenceColors, tCurColors, lux_check_enable)
+function inRange(tDUT, tCurColors, lux_check_enable)
 	local tTestSummary_device = {}
 	
 	local status, infotext, dnm, dsat, dlux
 	
 	for sensor = 1, 16 do 
 			tTestSummary_device[sensor] = {}
-			status, infotext, dnm, dsat, dlux = compareRows(tReferenceColors[sensor], tCurColors[sensor], lux_check_enable)
+			status, infotext, dnm, dsat, dlux = compareRows(tDUT[sensor], tCurColors[sensor], lux_check_enable)
 			tTestSummary_device[sensor].status = status
 			tTestSummary_device[sensor].infotext = infotext
 			tTestSummary_device[sensor].dnm  = dnm 
@@ -87,12 +93,13 @@ function inRange(tReferenceColors, tCurColors, lux_check_enable)
 end 
 
 
-function getDeviceSummary(tReferenceColors, tCurColors, lux_check_enable)
+function getDeviceSummary(tDUT, tCurColors, lux_check_enable)
 	
-	local tTestSummary_device = {}
+	local tTestSummary_device 
 	
-	if(tReferenceColors ~= nil) then 
-			  tTestSummary_device = inRange(tReferenceColors, tCurColors, lux_check_enable)
+	if(tDUT ~= nil) then 
+		tTestSummary_device = {}
+		tTestSummary_device = inRange(tDUT, tCurColors, lux_check_enable)
 	end 
 	
 	return tTestSummary_device 
@@ -119,9 +126,41 @@ end
 
 function validateTestSummary(numberOfDevices, tTestSummary)
 	-- Iterate over all device summaries
-	local devIndex = 0
+	local devIndex
 	local errorFlag = 0 
 	
+	-- check if any device entries are missing and warn if so --
+	devIndex = 0
+	while(devIndex < numberOfDevices) do 
+		if(tTestSummary[devIndex] == nil) then 
+			if WARNING_LEVEL == 1 then 
+				print(string.format("\n!!!!! WARNING !!!!!  WE HAVE %2d DEVICES BUT TEST ENTRY FOR DEVICE %d IS MISSING.\n", numberOfDevices, devIndex))
+			 
+			elseif WARNING_LEVEL == 2 then
+				print(string.format("\n!!!!! WARNING !!!!! WE HAVE %2d DEVICES BUT TEST ENTRY FOR DEVICE %d IS MISSING. QUITTING.\n", numberOfDevices, devIndex))
+				return TEST_RESULT_DEVICE_FAILED
+			end 
+		end 
+		devIndex = devIndex + 1 
+	end 
+	
+	-- check if any sensor entries under a device are missing and warn if so -- 
+	devIndex = 0 
+	while(devIndex < numberOfDevices) do 
+		if(tTestSummary[devIndex] ~= nil) then 
+			for i = 1, 16 do 
+				if(tTestSummary[devIndex][i].infotext == "NO TEST ENTRY\n") then 
+					if WARNING_LEVEL == 1 then 
+						print(string.format("!! WARNING !! NO TEST ENTRY FOR SENSOR %2d on DEVICE %2d", i, devIndex))
+					end 
+				end 
+			end 
+		end 
+		devIndex = devIndex + 1 
+	end 
+	
+	-- now validate all existent entries -- 
+	devIndex = 0
 	print("\n\n")
 	while(devIndex < numberOfDevices) do
 		if(tTestSummary[devIndex] ~= nil) then 
@@ -133,11 +172,14 @@ function validateTestSummary(numberOfDevices, tTestSummary)
 						print(tTestSummary[devIndex][i].infotext)					
 						errorFlag = 1
 					end 
-				end 
+				end  
 			end 
-		end 
+		end  
 		devIndex = devIndex + 1 
 	end
+	
+			
+		
 	
 	if errorFlag == 0 then 
 		print("")
@@ -149,7 +191,17 @@ function validateTestSummary(numberOfDevices, tTestSummary)
 		print("##     ## ##   ##  ")
 		print(" #######  ##    ## ")
 		print("")
+		
+		return TEST_RESULT_OK
+	else 
+		print("")
+		print(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		print(" !!!!!!!!!!!!!			 !!!!!!!!!!!!!")
+		print(" !!!!!!!!!!!!!       ERROR       !!!!!!!!!!!!!")
+		print(" !!!!!!!!!!!!!			 !!!!!!!!!!!!!")
+		print(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		print("")
+		return TEST_RESULT_LED_FAILED
 	end 
-	print("\n\n")
 	
 end  
