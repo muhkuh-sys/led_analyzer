1e09482435e1bb7fdf216138294e73ab7082aa7e
Subhan <swaizi@hilscher.com>
2015-07-28 16:48:20 +0200

    delete lua

--- lua/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/subhan/Desktop/led_analyzer/lua/lib
-# Build directory: C:/Users/subhan/Desktop/led_analyzer/lua/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(lua-5.1.4)
