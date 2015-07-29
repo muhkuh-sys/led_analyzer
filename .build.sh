feac6fb5a7ac0bb296420ff4b174a5302cfa2089
Subhan <swaizi@hilscher.com>
2015-07-29 16:27:00 +0200

    add ant dependency check

--- .build.sh
+++ .build.sh
@@ -41,8 +41,8 @@ cd ..
 ## -----------------------------------------
 #  Build the 64 Bit Version
 
-#rm -rf build_windows64
-#mkdir build_windows64
+rm -rf build_windows64
+mkdir build_windows64
 cd build_windows64
 
 cmake -DUSE_SYSTEM_LUA=FALSE -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wxwidgets/windows_amd64/bin/wx-config -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SHARED_LINKER_FLAGS=-m64 -DCMAKE_EXE_LINKER_FLAGS=-m64 -DCMAKE_MODULE_LINKER_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ -DCMAKE_PREFIX_PATH=${IVY_LIBRARY_FOLDER}/org/muhkuh/lua/lua51-5.1.3/windows_amd64/cmake ..
