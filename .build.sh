94d4dab3d906340f92c819bc28dd00a9e297cbb8
Subhan <swaizi@hilscher.com>
2015-07-29 16:11:46 +0200

    add ant dependency process

--- .build.sh
+++ .build.sh
@@ -13,11 +13,13 @@ popd
 IVY_LIBRARY_FOLDER=`pwd`/ivy/dependencies/lib
 
 
+
+
 ## -----------------------------------------
 #  Build the 32 Bit Version
 
-#rm -rf build_windows32
-#mkdir build_windows32
+rm -rf build_windows32
+mkdir build_windows32
 cd build_windows32
 
 cmake -DUSE_SYSTEM_LUA=FALSE -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wxwidgets/windows_x86/bin/wx-config -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 -DCMAKE_EXE_LINKER_FLAGS=-m32 -DCMAKE_MODULE_LINKER_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=/usr/bin/i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/i686-w64-mingw32-g++ -DCMAKE_PREFIX_PATH=${IVY_LIBRARY_FOLDER}/org/muhkuh/lua/lua51-5.1.3/windows_x86/cmake ..
