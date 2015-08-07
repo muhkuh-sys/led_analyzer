cc978943fcd679dbc9d5d0d652b86d40514d1029
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-08-07 17:49:39 +0200

    Get the path to the precompiled LUA package from the ANT system.

--- .build.sh
+++ .build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e
 
-#-----------------------------------------------------------------------------
-#
+## -----------------------------------------
 # Get the build requirements with ivy.
-#
 
 pushd ivy/dependencies
 ant bootstrap
 ant
 popd
-
-IVY_LIBRARY_FOLDER=`pwd`/ivy/dependencies/lib
 
 
 ## -----------------------------------------
@@ -20,7 +16,7 @@ IVY_LIBRARY_FOLDER=`pwd`/ivy/dependencies/lib
 rm -rf build_windows32
 mkdir build_windows32
 pushd build_windows32
-cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-i686/bin/wx-config -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 -DCMAKE_EXE_LINKER_FLAGS=-m32 -DCMAKE_MODULE_LINKER_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -DCMAKE_PREFIX_PATH=${IVY_LIBRARY_FOLDER}/org/muhkuh/lua/lua51-5.1.3/windows_x86/cmake ..
+cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-i686/bin/wx-config -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 -DCMAKE_EXE_LINKER_FLAGS=-m32 -DCMAKE_MODULE_LINKER_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ ..
 make
 popd
 
@@ -31,7 +27,7 @@ popd
 rm -rf build_windows64
 mkdir build_windows64
 pushd build_windows64
-cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-x86_64/bin/wx-config -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SHARED_LINKER_FLAGS=-m64 -DCMAKE_EXE_LINKER_FLAGS=-m64 -DCMAKE_MODULE_LINKER_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_PREFIX_PATH=${IVY_LIBRARY_FOLDER}/org/muhkuh/lua/lua51-5.1.3/windows_amd64/cmake ..
+cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-x86_64/bin/wx-config -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SHARED_LINKER_FLAGS=-m64 -DCMAKE_EXE_LINKER_FLAGS=-m64 -DCMAKE_MODULE_LINKER_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ ..
 make
 popd
 
