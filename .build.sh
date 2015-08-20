c02998257df89ccc4eef05356f04b3f8af1599b2
Subhan <swaizi@hilscher.com>
2015-08-20 10:54:40 +0200

    add resource windres compiler

--- .build.sh
+++ .build.sh
@@ -16,7 +16,7 @@ popd
 rm -rf build_windows32
 mkdir build_windows32
 pushd build_windows32
-cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-i686/bin/wx-config -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 -DCMAKE_EXE_LINKER_FLAGS=-m32 -DCMAKE_MODULE_LINKER_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ ..
+cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-i686/bin/wx-config -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 -DCMAKE_EXE_LINKER_FLAGS=-m32 -DCMAKE_MODULE_LINKER_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=i686-w64-mingw32-windres ..
 make
 popd
 
@@ -27,7 +27,7 @@ popd
 rm -rf build_windows64
 mkdir build_windows64
 pushd build_windows64
-cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-x86_64/bin/wx-config -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SHARED_LINKER_FLAGS=-m64 -DCMAKE_EXE_LINKER_FLAGS=-m64 -DCMAKE_MODULE_LINKER_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ ..
+cmake -DwxWidgets_CONFIG_EXECUTABLE=/opt/mingw-w64-x86_64/bin/wx-config -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SHARED_LINKER_FLAGS=-m64 -DCMAKE_EXE_LINKER_FLAGS=-m64 -DCMAKE_MODULE_LINKER_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres ..
 make
 popd
 
@@ -42,4 +42,3 @@ cmake ../ivy/installer
 make
 make install
 popd
-
